2ec5b1e8251d6d4d114762481600f9399a5babd3
MattColaProgrammer93 <mattchoque115@gmail.com>
2022-08-02 11:18:04 -0700

    Renamed the radiobuttons to chosen crusts and added 8 checkboxes to the Toppings groupbox

--- MamaJohn'sPizza/Order.Designer.cs
+++ MamaJohn'sPizza/Order.Designer.cs
@@ -35,11 +35,20 @@
             this.btnExit = new System.Windows.Forms.Button();
             this.rdoCircleShape = new System.Windows.Forms.RadioButton();
             this.rdoSquareShape = new System.Windows.Forms.RadioButton();
-            this.radioButton3 = new System.Windows.Forms.RadioButton();
-            this.radioButton4 = new System.Windows.Forms.RadioButton();
-            this.radioButton5 = new System.Windows.Forms.RadioButton();
-            this.radioButton6 = new System.Windows.Forms.RadioButton();
+            this.rdoStuffedCrust = new System.Windows.Forms.RadioButton();
+            this.rdoCrackerCrust = new System.Windows.Forms.RadioButton();
+            this.rdoThinCrust = new System.Windows.Forms.RadioButton();
+            this.rdoFlatbread = new System.Windows.Forms.RadioButton();
+            this.checkBox1 = new System.Windows.Forms.CheckBox();
+            this.checkBox2 = new System.Windows.Forms.CheckBox();
+            this.checkBox3 = new System.Windows.Forms.CheckBox();
+            this.checkBox4 = new System.Windows.Forms.CheckBox();
+            this.checkBox5 = new System.Windows.Forms.CheckBox();
+            this.checkBox6 = new System.Windows.Forms.CheckBox();
+            this.checkBox7 = new System.Windows.Forms.CheckBox();
+            this.checkBox8 = new System.Windows.Forms.CheckBox();
             this.grpPizzaShapes.SuspendLayout();
+            this.grpToppings.SuspendLayout();
             this.grpCrustTypes.SuspendLayout();
             this.SuspendLayout();
             // 
@@ -56,6 +65,14 @@
             // 
             // grpToppings
             // 
+            this.grpToppings.Controls.Add(this.checkBox8);
+            this.grpToppings.Controls.Add(this.checkBox7);
+            this.grpToppings.Controls.Add(this.checkBox6);
+            this.grpToppings.Controls.Add(this.checkBox5);
+            this.grpToppings.Controls.Add(this.checkBox4);
+            this.grpToppings.Controls.Add(this.checkBox3);
+            this.grpToppings.Controls.Add(this.checkBox2);
+            this.grpToppings.Controls.Add(this.checkBox1);
             this.grpToppings.Location = new System.Drawing.Point(330, 37);
             this.grpToppings.Name = "grpToppings";
             this.grpToppings.Size = new System.Drawing.Size(250, 176);
@@ -65,10 +82,10 @@
             // 
             // grpCrustTypes
             // 
-            this.grpCrustTypes.Controls.Add(this.radioButton6);
-            this.grpCrustTypes.Controls.Add(this.radioButton5);
-            this.grpCrustTypes.Controls.Add(this.radioButton4);
-            this.grpCrustTypes.Controls.Add(this.radioButton3);
+            this.grpCrustTypes.Controls.Add(this.rdoFlatbread);
+            this.grpCrustTypes.Controls.Add(this.rdoThinCrust);
+            this.grpCrustTypes.Controls.Add(this.rdoCrackerCrust);
+            this.grpCrustTypes.Controls.Add(this.rdoStuffedCrust);
             this.grpCrustTypes.Location = new System.Drawing.Point(34, 165);
             this.grpCrustTypes.Name = "grpCrustTypes";
             this.grpCrustTypes.Size = new System.Drawing.Size(250, 152);
@@ -116,49 +133,129 @@
             this.rdoSquareShape.Text = "Square ($8.00)";
             this.rdoSquareShape.UseVisualStyleBackColor = true;
             // 
-            // radioButton3
+            // rdoStuffedCrust
             // 
-            this.radioButton3.AutoSize = true;
-            this.radioButton3.Location = new System.Drawing.Point(19, 26);
-            this.radioButton3.Name = "radioButton3";
-            this.radioButton3.Size = new System.Drawing.Size(117, 24);
-            this.radioButton3.TabIndex = 0;
-            this.radioButton3.TabStop = true;
-            this.radioButton3.Text = "radioButton3";
-            this.radioButton3.UseVisualStyleBackColor = true;
+            this.rdoStuffedCrust.AutoSize = true;
+            this.rdoStuffedCrust.Location = new System.Drawing.Point(19, 26);
+            this.rdoStuffedCrust.Name = "rdoStuffedCrust";
+            this.rdoStuffedCrust.Size = new System.Drawing.Size(115, 24);
+            this.rdoStuffedCrust.TabIndex = 0;
+            this.rdoStuffedCrust.TabStop = true;
+            this.rdoStuffedCrust.Text = "Stuffed Crust";
+            this.rdoStuffedCrust.UseVisualStyleBackColor = true;
             // 
-            // radioButton4
+            // rdoCrackerCrust
             // 
-            this.radioButton4.AutoSize = true;
-            this.radioButton4.Location = new System.Drawing.Point(19, 56);
-            this.radioButton4.Name = "radioButton4";
-            this.radioButton4.Size = new System.Drawing.Size(117, 24);
-            this.radioButton4.TabIndex = 1;
-            this.radioButton4.TabStop = true;
-            this.radioButton4.Text = "radioButton4";
-            this.radioButton4.UseVisualStyleBackColor = true;
+            this.rdoCrackerCrust.AutoSize = true;
+            this.rdoCrackerCrust.Location = new System.Drawing.Point(19, 56);
+            this.rdoCrackerCrust.Name = "rdoCrackerCrust";
+            this.rdoCrackerCrust.Size = new System.Drawing.Size(116, 24);
+            this.rdoCrackerCrust.TabIndex = 1;
+            this.rdoCrackerCrust.TabStop = true;
+            this.rdoCrackerCrust.Text = "Cracker Crust";
+            this.rdoCrackerCrust.UseVisualStyleBackColor = true;
             // 
-            // radioButton5
+            // rdoThinCrust
             // 
-            this.radioButton5.AutoSize = true;
-            this.radioButton5.Location = new System.Drawing.Point(19, 86);
-            this.radioButton5.Name = "radioButton5";
-            this.radioButton5.Size = new System.Drawing.Size(117, 24);
-            this.radioButton5.TabIndex = 2;
-            this.radioButton5.TabStop = true;
-            this.radioButton5.Text = "radioButton5";
-            this.radioButton5.UseVisualStyleBackColor = true;
+            this.rdoThinCrust.AutoSize = true;
+            this.rdoThinCrust.Location = new System.Drawing.Point(19, 86);
+            this.rdoThinCrust.Name = "rdoThinCrust";
+            this.rdoThinCrust.Size = new System.Drawing.Size(95, 24);
+            this.rdoThinCrust.TabIndex = 2;
+            this.rdoThinCrust.TabStop = true;
+            this.rdoThinCrust.Text = "Thin Crust";
+            this.rdoThinCrust.UseVisualStyleBackColor = true;
             // 
-            // radioButton6
+            // rdoFlatbread
             // 
-            this.radioButton6.AutoSize = true;
-            this.radioButton6.Location = new System.Drawing.Point(19, 116);
-            this.radioButton6.Name = "radioButton6";
-            this.radioButton6.Size = new System.Drawing.Size(117, 24);
-            this.radioButton6.TabIndex = 3;
-            this.radioButton6.TabStop = true;
-            this.radioButton6.Text = "radioButton6";
-            this.radioButton6.UseVisualStyleBackColor = true;
+            this.rdoFlatbread.AutoSize = true;
+            this.rdoFlatbread.Location = new System.Drawing.Point(19, 116);
+            this.rdoFlatbread.Name = "rdoFlatbread";
+            this.rdoFlatbread.Size = new System.Drawing.Size(130, 24);
+            this.rdoFlatbread.TabIndex = 3;
+            this.rdoFlatbread.TabStop = true;
+            this.rdoFlatbread.Text = "Flatbread Crust";
+            this.rdoFlatbread.UseVisualStyleBackColor = true;
+            // 
+            // checkBox1
+            // 
+            this.checkBox1.AutoSize = true;
+            this.checkBox1.Location = new System.Drawing.Point(6, 27);
+            this.checkBox1.Name = "checkBox1";
+            this.checkBox1.Size = new System.Drawing.Size(101, 24);
+            this.checkBox1.TabIndex = 5;
+            this.checkBox1.Text = "checkBox1";
+            this.checkBox1.UseVisualStyleBackColor = true;
+            // 
+            // checkBox2
+            // 
+            this.checkBox2.AutoSize = true;
+            this.checkBox2.Location = new System.Drawing.Point(6, 57);
+            this.checkBox2.Name = "checkBox2";
+            this.checkBox2.Size = new System.Drawing.Size(101, 24);
+            this.checkBox2.TabIndex = 6;
+            this.checkBox2.Text = "checkBox2";
+            this.checkBox2.UseVisualStyleBackColor = true;
+            // 
+            // checkBox3
+            // 
+            this.checkBox3.AutoSize = true;
+            this.checkBox3.Location = new System.Drawing.Point(6, 87);
+            this.checkBox3.Name = "checkBox3";
+            this.checkBox3.Size = new System.Drawing.Size(101, 24);
+            this.checkBox3.TabIndex = 7;
+            this.checkBox3.Text = "checkBox3";
+            this.checkBox3.UseVisualStyleBackColor = true;
+            // 
+            // checkBox4
+            // 
+            this.checkBox4.AutoSize = true;
+            this.checkBox4.Location = new System.Drawing.Point(6, 117);
+            this.checkBox4.Name = "checkBox4";
+            this.checkBox4.Size = new System.Drawing.Size(101, 24);
+            this.checkBox4.TabIndex = 8;
+            this.checkBox4.Text = "checkBox4";
+            this.checkBox4.UseVisualStyleBackColor = true;
+            // 
+            // checkBox5
+            // 
+            this.checkBox5.AutoSize = true;
+            this.checkBox5.Location = new System.Drawing.Point(113, 27);
+            this.checkBox5.Name = "checkBox5";
+            this.checkBox5.Size = new System.Drawing.Size(101, 24);
+            this.checkBox5.TabIndex = 9;
+            this.checkBox5.Text = "checkBox5";
+            this.checkBox5.UseVisualStyleBackColor = true;
+            // 
+            // checkBox6
+            // 
+            this.checkBox6.AutoSize = true;
+            this.checkBox6.Location = new System.Drawing.Point(113, 57);
+            this.checkBox6.Name = "checkBox6";
+            this.checkBox6.Size = new System.Drawing.Size(101, 24);
+            this.checkBox6.TabIndex = 10;
+            this.checkBox6.Text = "checkBox6";
+            this.checkBox6.UseVisualStyleBackColor = true;
+            // 
+            // checkBox7
+            // 
+            this.checkBox7.AutoSize = true;
+            this.checkBox7.Location = new System.Drawing.Point(113, 87);
+            this.checkBox7.Name = "checkBox7";
+            this.checkBox7.Size = new System.Drawing.Size(101, 24);
+            this.checkBox7.TabIndex = 11;
+            this.checkBox7.Text = "checkBox7";
+            this.checkBox7.UseVisualStyleBackColor = true;
+            // 
+            // checkBox8
+            // 
+            this.checkBox8.AutoSize = true;
+            this.checkBox8.Location = new System.Drawing.Point(113, 117);
+            this.checkBox8.Name = "checkBox8";
+            this.checkBox8.Size = new System.Drawing.Size(101, 24);
+            this.checkBox8.TabIndex = 12;
+            this.checkBox8.Text = "checkBox8";
+            this.checkBox8.UseVisualStyleBackColor = true;
             // 
             // Order
             // 
@@ -174,6 +271,8 @@
             this.Text = "Order";
             this.grpPizzaShapes.ResumeLayout(false);
             this.grpPizzaShapes.PerformLayout();
+            this.grpToppings.ResumeLayout(false);
+            this.grpToppings.PerformLayout();
             this.grpCrustTypes.ResumeLayout(false);
             this.grpCrustTypes.PerformLayout();
             this.ResumeLayout(false);
@@ -189,9 +288,17 @@
         private Button btnExit;
         private RadioButton rdoSquareShape;
         private RadioButton rdoCircleShape;
-        private RadioButton radioButton6;
-        private RadioButton radioButton5;
-        private RadioButton radioButton4;
-        private RadioButton radioButton3;
+        private RadioButton rdoFlatbread;
+        private RadioButton rdoThinCrust;
+        private RadioButton rdoCrackerCrust;
+        private RadioButton rdoStuffedCrust;
+        private CheckBox checkBox8;
+        private CheckBox checkBox7;
+        private CheckBox checkBox6;
+        private CheckBox checkBox5;
+        private CheckBox checkBox4;
+        private CheckBox checkBox3;
+        private CheckBox checkBox2;
+        private CheckBox checkBox1;
     }
 }
