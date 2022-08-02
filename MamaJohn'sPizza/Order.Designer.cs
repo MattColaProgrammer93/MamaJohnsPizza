551c4e37b28817ced401f38035d6443d5d67550d
MattColaProgrammer93 <mattchoque115@gmail.com>
2022-08-02 11:13:48 -0700

    Added the Shapes for the Pizza Shapes groupbox and added the other four radiobuttons in Crust Types

--- MamaJohn'sPizza/Order.Designer.cs
+++ MamaJohn'sPizza/Order.Designer.cs
@@ -33,13 +33,23 @@
             this.grpCrustTypes = new System.Windows.Forms.GroupBox();
             this.btnOrder = new System.Windows.Forms.Button();
             this.btnExit = new System.Windows.Forms.Button();
+            this.rdoCircleShape = new System.Windows.Forms.RadioButton();
+            this.rdoSquareShape = new System.Windows.Forms.RadioButton();
+            this.radioButton3 = new System.Windows.Forms.RadioButton();
+            this.radioButton4 = new System.Windows.Forms.RadioButton();
+            this.radioButton5 = new System.Windows.Forms.RadioButton();
+            this.radioButton6 = new System.Windows.Forms.RadioButton();
+            this.grpPizzaShapes.SuspendLayout();
+            this.grpCrustTypes.SuspendLayout();
             this.SuspendLayout();
             // 
             // grpPizzaShapes
             // 
+            this.grpPizzaShapes.Controls.Add(this.rdoSquareShape);
+            this.grpPizzaShapes.Controls.Add(this.rdoCircleShape);
             this.grpPizzaShapes.Location = new System.Drawing.Point(34, 37);
             this.grpPizzaShapes.Name = "grpPizzaShapes";
-            this.grpPizzaShapes.Size = new System.Drawing.Size(250, 125);
+            this.grpPizzaShapes.Size = new System.Drawing.Size(250, 97);
             this.grpPizzaShapes.TabIndex = 0;
             this.grpPizzaShapes.TabStop = false;
             this.grpPizzaShapes.Text = "Pizza Shapes";
@@ -55,12 +65,16 @@
             // 
             // grpCrustTypes
             // 
-            this.grpCrustTypes.Location = new System.Drawing.Point(34, 189);
+            this.grpCrustTypes.Controls.Add(this.radioButton6);
+            this.grpCrustTypes.Controls.Add(this.radioButton5);
+            this.grpCrustTypes.Controls.Add(this.radioButton4);
+            this.grpCrustTypes.Controls.Add(this.radioButton3);
+            this.grpCrustTypes.Location = new System.Drawing.Point(34, 165);
             this.grpCrustTypes.Name = "grpCrustTypes";
-            this.grpCrustTypes.Size = new System.Drawing.Size(250, 172);
+            this.grpCrustTypes.Size = new System.Drawing.Size(250, 152);
             this.grpCrustTypes.TabIndex = 2;
             this.grpCrustTypes.TabStop = false;
-            this.grpCrustTypes.Text = "Crusts";
+            this.grpCrustTypes.Text = "Crust Types";
             // 
             // btnOrder
             // 
@@ -80,6 +94,72 @@
             this.btnExit.Text = "Exit";
             this.btnExit.UseVisualStyleBackColor = true;
             // 
+            // rdoCircleShape
+            // 
+            this.rdoCircleShape.AutoSize = true;
+            this.rdoCircleShape.Location = new System.Drawing.Point(19, 26);
+            this.rdoCircleShape.Name = "rdoCircleShape";
+            this.rdoCircleShape.Size = new System.Drawing.Size(116, 24);
+            this.rdoCircleShape.TabIndex = 0;
+            this.rdoCircleShape.TabStop = true;
+            this.rdoCircleShape.Text = "Circle ($7.25)";
+            this.rdoCircleShape.UseVisualStyleBackColor = true;
+            // 
+            // rdoSquareShape
+            // 
+            this.rdoSquareShape.AutoSize = true;
+            this.rdoSquareShape.Location = new System.Drawing.Point(19, 56);
+            this.rdoSquareShape.Name = "rdoSquareShape";
+            this.rdoSquareShape.Size = new System.Drawing.Size(125, 24);
+            this.rdoSquareShape.TabIndex = 1;
+            this.rdoSquareShape.TabStop = true;
+            this.rdoSquareShape.Text = "Square ($8.00)";
+            this.rdoSquareShape.UseVisualStyleBackColor = true;
+            // 
+            // radioButton3
+            // 
+            this.radioButton3.AutoSize = true;
+            this.radioButton3.Location = new System.Drawing.Point(19, 26);
+            this.radioButton3.Name = "radioButton3";
+            this.radioButton3.Size = new System.Drawing.Size(117, 24);
+            this.radioButton3.TabIndex = 0;
+            this.radioButton3.TabStop = true;
+            this.radioButton3.Text = "radioButton3";
+            this.radioButton3.UseVisualStyleBackColor = true;
+            // 
+            // radioButton4
+            // 
+            this.radioButton4.AutoSize = true;
+            this.radioButton4.Location = new System.Drawing.Point(19, 56);
+            this.radioButton4.Name = "radioButton4";
+            this.radioButton4.Size = new System.Drawing.Size(117, 24);
+            this.radioButton4.TabIndex = 1;
+            this.radioButton4.TabStop = true;
+            this.radioButton4.Text = "radioButton4";
+            this.radioButton4.UseVisualStyleBackColor = true;
+            // 
+            // radioButton5
+            // 
+            this.radioButton5.AutoSize = true;
+            this.radioButton5.Location = new System.Drawing.Point(19, 86);
+            this.radioButton5.Name = "radioButton5";
+            this.radioButton5.Size = new System.Drawing.Size(117, 24);
+            this.radioButton5.TabIndex = 2;
+            this.radioButton5.TabStop = true;
+            this.radioButton5.Text = "radioButton5";
+            this.radioButton5.UseVisualStyleBackColor = true;
+            // 
+            // radioButton6
+            // 
+            this.radioButton6.AutoSize = true;
+            this.radioButton6.Location = new System.Drawing.Point(19, 116);
+            this.radioButton6.Name = "radioButton6";
+            this.radioButton6.Size = new System.Drawing.Size(117, 24);
+            this.radioButton6.TabIndex = 3;
+            this.radioButton6.TabStop = true;
+            this.radioButton6.Text = "radioButton6";
+            this.radioButton6.UseVisualStyleBackColor = true;
+            // 
             // Order
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 20F);
@@ -92,6 +172,10 @@
             this.Controls.Add(this.grpPizzaShapes);
             this.Name = "Order";
             this.Text = "Order";
+            this.grpPizzaShapes.ResumeLayout(false);
+            this.grpPizzaShapes.PerformLayout();
+            this.grpCrustTypes.ResumeLayout(false);
+            this.grpCrustTypes.PerformLayout();
             this.ResumeLayout(false);
 
         }
@@ -103,5 +187,11 @@
         private GroupBox grpCrustTypes;
         private Button btnOrder;
         private Button btnExit;
+        private RadioButton rdoSquareShape;
+        private RadioButton rdoCircleShape;
+        private RadioButton radioButton6;
+        private RadioButton radioButton5;
+        private RadioButton radioButton4;
+        private RadioButton radioButton3;
     }
 }
