5eb174c463fc4c401de6dc798931148483bd1f2c
MattColaProgrammer93 <mattchoque115@gmail.com>
2022-08-16 11:16:14 -0700

    Constructed the PizzaOrderContext to create a database

---
+++ MamaJohn'sPizza/PizzaOrderContext.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.ComponentModel.DataAnnotations;
+using Microsoft.EntityFrameworkCore;
+
+namespace MamaJohn_sPizza
+{
+    public class PizzaOrderContext : DbContext
+    {
+        public PizzaOrderContext()
+        {
+
+        }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder options)
+        {
+            options.UseSqlServer("Server=(localdb)\\mssqllocaldb;Database=MamaJohnsPizza;Trusted_Connection=True;");
+        }
+
+        public DbSet<Pizza> Pizzas { get; set; }
+    }
+}
